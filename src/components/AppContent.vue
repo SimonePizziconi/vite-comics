8959b0dbaeb849a0dcb4992bb21201cfae763624
SimonePizziconi <simonepizziconi05@gmail.com>
2024-07-05 16:35:03 +0200

    feat: add component in main

---
+++ src/components/AppContent.vue
@@ -0,0 +1,31 @@
+<script>
+export default{
+    name: `AppContent`,
+}
+</script>
+
+<template>
+    <section>
+        <div class="content">
+            <h1> content go here <-- </h1>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+    @use "../styles/partials/mixins" as *;
+
+    section{
+        background-color: #000;
+        @include centerFlex();
+        justify-content: flex-start;
+        // DEBUG
+        min-height: 100px;
+
+        .content{
+            width: 70%;
+            margin: 0 auto;
+        }
+
+    }
+</style>
