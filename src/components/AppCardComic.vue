fca4c5156e1086e851288e42e3bd2fef9147372a
SimonePizziconi <simonepizziconi05@gmail.com>
2024-07-08 15:56:29 +0200

    feat: create component AppCardComic

---
+++ src/components/AppCardComic.vue
@@ -0,0 +1,32 @@
+<script>
+export default{
+    name: `AppCardComic`,
+    props: {
+        details: Object
+    }
+}
+</script>
+
+<template>
+    <div>
+        <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="Fumetto">
+        <h3>Action comic</h3>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+    div{
+        width: calc(100% / 6);
+
+        img{
+        width: 100%;
+        }
+
+        h3{
+            text-align: center;
+        }
+    }
+
+   
+</style>
